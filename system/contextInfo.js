b0175ab3171b8776025160a9e35a68431727d848
GOD-Da <dawiniefleury17@gmail.com>
2025-10-25 01:32:48 -0400

    Add files via upload

---
+++ system/contextInfo.js
@@ -0,0 +1,10 @@
+// ==================== system/contextInfo.js ====================
+export const contextInfo = {
+  forwardingScore: 999,
+  isForwarded: true,
+  forwardedNewsletterMessageInfo: {
+    newsletterJid: '120363419768812867@newsletter',
+    newsletterName: '𝐃𝐄𝐕. 𝐃𝐀𝐖𝐄𝐍𝐒 ☩ || 𝐎𝐅𝐅𝐂',
+    serverMessageId: 150
+  }
+};
